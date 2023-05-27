6bc720484dac6d426022f180da255603729815b4
vira-he <vira.connect@gmail.com>
2023-05-27 12:07:55 -0500

    HW task Make a test case with a loop. completed

---
+++ features/steps/amazon_product_page.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+
+
+COLOR_OPTIONS = By.CSS_SELECTOR, ".imgSwatch"
+
+
+@given("Open amazon product B07BJKRR25 page")
+def open_amazon_product(context):
+    context.driver.get('https://www.amazon.com/gp/product/B07BJL37GD')
+
+
+@then("Verify user can click through colors")
+def verify_can_click_colors(context):
+    expected_colors = ['Black', 'Blue, Over Dye', 'Bright White']
+    actual_colors = []
+
+    colors = context.driver.find_elements(*COLOR_OPTIONS)
+
+    for color in colors [:3]:
+        color.click()
+        current_color = context.driver.find_element(By.CSS_SELECTOR,".selection").text
+        actual_colors += [current_color]
+
+    assert expected_colors == actual_colors, \
+        f'Expected colors {expected_colors} did not match {actual_colors}'
---
+++ features/tests/amazon_product_details.feature
@@ -0,0 +1,6 @@
+# Created by vera at 5/27/23
+Feature: Product page with multiple elements
+
+  Scenario: User can select colors
+    Given Open amazon product B07BJKRR25 page
+    Then Verify user can click through colors
